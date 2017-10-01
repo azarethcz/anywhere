2741dc97023e42107626a9e950ffd5bea030b8c9
kiCkZ <kiCkZ@10.0.0.1>
2017-10-01 13:54:56 +0200

    Anywhere Wordpress Script

---
+++ anywhere_project/wordpress_anywhere.php
@@ -0,0 +1,17 @@
+<?php 
+    $request = wp_remote_get('http://my.adamapp.com/export/6490.json');
+        if(is_wp_error($request)) {
+                return false;
+        }
+        $body = wp_remote_retrieve_body($request);
+        $export = json_decode($body);
+        if(!empty($export)) {
+            foreach($export->result->data->tab as $article) {
+                echo '<article id="'.$article->id.'">';
+                        echo '<h3>'.$article->type->name.'</h3>';                                      
+                        echo '<span>'.$article->published_date.'</span>';                      
+                echo '</article>';
+            }
+    }
+    
+?>
